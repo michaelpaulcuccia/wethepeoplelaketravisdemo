fb7fe3509e28dafcc1590ba110af96a075bef642
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-05 11:05:27 -0500

    progress capture, new approach

--- my-app/Components/NavBar.js
+++ my-app/Components/NavBar.js
@@ -1,12 +1,11 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 import styled from "styled-components";
 import CustomLink from "./CustomLink";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { MdMail } from "react-icons/md";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { mobileBreakPoint } from "../Constants/constants";
+//import { GiHamburgerMenu } from "react-icons/gi";
+//import { mobileBreakPoint } from "../Constants/constants";
 
 const FixedPosition = styled.div`
   overflow: hidden;
@@ -17,88 +16,55 @@ const FixedPosition = styled.div`
 `;
 
 const Root = styled.div`
+  display: flex;
+  margin: 0 auto;
+  max-width: 1600px;
   color: white;
   background: blue;
   padding: 18px 8px;
-  max-width: 1600px;
-  margin: 0 auto;
-  display: flex;
 `;
 
-const DesktopDisplay = styled.div`
+const DesktopContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
-
-  @media (max-width: ${mobileBreakPoint}) {
-    display: none;
-  }
 `;
 
-const NavItem = styled.div`
+const LeftSideDesktop = styled.div`
   display: flex;
-  cursor: pointer;
-  margin-right: 18px;
+  width: 50%;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 8px;
 
   span {
-    margin-left: 6px;
-  }
-
-  &:hover {
-    color: #c0c0c0;
+    margin-left: 4px;
+    white-space: nowrap;
   }
 `;
 
-const LeftSideLinks = styled.div`
-  width: 50%;
-  display: flex;
-`;
-
-const RightSideLinks = styled.div`
+const RightideDesktop = styled.div`
   display: flex;
   justify-content: flex-end;
-  gap: 16px;
   width: 50%;
-
-  a {
-    &:hover {
-      color: #c0c0c0;
-    }
-  }
-`;
-
-const MobileDisplay = styled.div`
-  display: none;
-
-  @media (max-width: ${mobileBreakPoint}) {
-    display: block;
-  }
-`;
-
-const MobileContainer = styled.div`
-  diplay: flex;
-  width: 100%;
-  background: green;
-  justify-content: flex-end;
-  margin-left: auto;
 `;
 
 export default function NavBar() {
   return (
     <FixedPosition>
       <Root>
-        {/* DESKTOP STARTS */}
-        <DesktopDisplay>
-          <LeftSideLinks>
-            <NavItem>
+        <DesktopContainer>
+          <LeftSideDesktop>
+            <LinkContainer>
               <FaMapMarkerAlt />
               <span>Lakeway, Texas 78734</span>
-            </NavItem>
-
-            <NavItem>
+            </LinkContainer>
+            <LinkContainer>
               <MdMail />
               <span>
+                {" "}
                 <CustomLink
                   isemail={true}
                   href="info@wethepeoplelaketravis.com"
@@ -106,23 +72,24 @@ export default function NavBar() {
                   info@wethepeoplelaketravis.com
                 </CustomLink>
               </span>
-            </NavItem>
-          </LeftSideLinks>
-          <RightSideLinks>
-            <CustomLink href="/">DONATE NOW</CustomLink>
-            <CustomLink href="/">Register</CustomLink>
-            <CustomLink href="/">Login</CustomLink>
-          </RightSideLinks>
-        </DesktopDisplay>
-        {/* DESKTOP ENDS */}
-        {/* ********************************************** */}
-        {/* MOBILE BEGINS  */}
-        <MobileDisplay>
-          <MobileContainer>
-            <GiHamburgerMenu />
-          </MobileContainer>
-        </MobileDisplay>
-        {/* MOBILE ENDS  */}
+
+              {/* <div style={{ display: "flex" }}>
+                <MdMail />
+                <CustomLink
+                  isemail={true}
+                  href="info@wethepeoplelaketravis.com"
+                >
+                  info@wethepeoplelaketravis.com
+                </CustomLink>
+              </div> */}
+            </LinkContainer>
+          </LeftSideDesktop>
+          <RightideDesktop>
+            <div>DONATE NOW</div>
+            <div>Register</div>
+            <div>Login</div>
+          </RightideDesktop>
+        </DesktopContainer>
       </Root>
     </FixedPosition>
   );
